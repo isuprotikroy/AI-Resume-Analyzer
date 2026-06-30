6b896192bf1fa2438fa5b71be40d5f28db34c187
Suprotik Roy <suprotikroy@Suprotiks-MacBook-Air.local>
2026-06-30 12:10:47 +0530

    Initial React project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>HireMind AI</h1>
+
+      <p>AI Powered Resume Analyzer & Interview Assistant</p>
+
+      <button>Upload Resume</button>
+    </div>
+  )
+}
+
+export default App
